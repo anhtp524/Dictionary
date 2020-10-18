23085b5890335fa886f53bdde6ac62dbe61d1c9e
nhn042 <62633199+nhn042@users.noreply.github.com>
2020-10-18 11:40:18 +0700

    Update DictionaryManagement.java

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -21,12 +21,12 @@ public class DictionaryManagement {
     }
 
     public void insertFromFile() throws IOException {
-        File f = new File("C:\\Users\\anhtp\\IdeaProjects\\Dictionary\\tu_dien.txt");
+        File f = new File("C:\\Users\\Admin\\IdeaProjects\\Từ Điển\\test");
         FileReader fr = new FileReader(f);
         BufferedReader br = new BufferedReader(fr);
         String line;
         while((line = br.readLine()) != null) {
-            String[] s = line.split("\t");
+            String[] s = line.split("\n");
             Word c = new Word(s[0],s[1]);
             dictionary.add(c);
         }
@@ -34,32 +34,58 @@ public class DictionaryManagement {
         br.close();
     }
 
-    public void dictionaryLookup() {
-         Scanner sc = new Scanner (System.in);
-         String s = sc.nextLine();
-         for (int i = 0; i < dictionary.size(); i++) {
-             if (dictionary.getWordinList(i).equals(s)) {
+    public void dictionaryLookup() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            if (s1.equals(dictionary.getWordinList(i))) {
                 System.out.println(dictionary.getMeaninList(i));
                 break;
-             }
-         }
+            }
+        }
     }
 
-    public void addWord() {
+    public void addWord() throws IOException {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
         String s2 = sc.nextLine();
-        Word newWord = new Word(s1, s2);
-        dictionary.add(newWord);
+        int n = sc.nextInt();
+        Word w = new Word(s1, s2);
+        dictionary.add(w);
     }
 
-    public void deleteWord() {
+    public void removeWord() throws IOException {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
-        dictionary.delete(s1);
+        String s2 = sc.nextLine();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            if(dictionary.getWordinList(i).equals(s1) && dictionary.getMeaninList(i).equals(s2)) {
+                for(int j = i; j < n - 1; j++) {
+                    dictionary.getWordinList(j).equals(dictionary.getWordinList(j+1));
+                    dictionary.getMeaninList(j).equals(dictionary.getMeaninList(j+1));
+                }
+            }
+            Word w = new Word(s1, s2);
+            dictionary.delete(w);
+        }
     }
 
-
+    public void fixWord() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = sc.nextLine();
+        String s4 = sc.nextLine();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            if(dictionary.getWordinList(i).equals(s1) && dictionary.getMeaninList(i).equals(s2)) {
+                dictionary.getWordinList(i).equals(s3);
+                dictionary.getMeaninList(i).equals(s4);
+            }
+        }
+    }
 
 
 
