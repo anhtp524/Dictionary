4daff91890c3f88ad581b4fa48e2a31f83686427
anhtp524 <61215691+anhtp524@users.noreply.github.com>
2020-10-26 22:22:23 +0700

    comandline

--- Dictionary.java
+++ Dictionary.java
@@ -1,34 +1,42 @@
 import java.util.ArrayList;
 
 public class Dictionary {
-    public ArrayList<Word> list = new ArrayList<>();
+     private ArrayList<Word> list = new ArrayList<>();
+     private int count = 0;
 
-    public void add(Word a) {
-        list.add(a);
-    }
+     public void add(Word a) {
+         list.add(a);
+         count ++;
+     }
 
     public void delete(String a) {
-        for (int i = 0; i < list.size(); i++) {
+         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getWord().equals(a)) {
                 list.remove(i);
+                count --;
             }
-        }
+         }
 
+    }
+
+    public int size() {
+         return count;
     }
 
     public String getWordinList(int n) {
-        return list.get(n).getWord();
+         return list.get(n).getWord();
     }
 
     public String getMeaninList(int n) {
-        return list.get(n).getMean();
+         return list.get(n).getMean();
     }
 
     public void fix(String word, String input) {
-        for (int i = 0; i < list.size(); i++) {
-            if(getWordinList(i).equals(word) == true) {
-                list.get(i).setMean(input);
-            }
-        }
+         for (int i = 0; i < list.size(); i++) {
+             if(getWordinList(i).equals(word) == true) {
+                 list.get(i).setMean(input);
+             }
+         }
     }
 }
+
